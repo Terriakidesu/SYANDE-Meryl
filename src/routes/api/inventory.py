dd8e5b4e24052df912bd96bc1e5b01c4c12811cd
Terriakidesu <trra53@gmail.com>
2025-12-24 19:04:59 +0800

    api/inventory routes

--- src/routes/api/inventory.py
+++ src/routes/api/inventory.py
@@ -1,4 +1,6 @@
 
+from typing import Optional
+
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 
@@ -9,9 +11,15 @@ inventory_router = APIRouter(prefix="/api/inventory")
 db = Database()
 
 
-@inventory_router.get("/items", response_class=JSONResponse)
+@inventory_router.get("/", response_class=JSONResponse)
 async def list_items(request: Request):
+    return db.fetchAll(r"SELECT * FROM products")
 
-    # db.connect()
 
-    return db.fetchAll("SELECT * from Products")
+@inventory_router.get("/{product_id}", response_class=JSONResponse)
+async def list_item(request: Request, product_id: Optional[int] = None):
+
+    if product_id is None:
+        return []
+
+    return db.fetchAll(r"SELECT * FROM products WHERE product_id = %s", (product_id,))
