9cf600cd6864c3825700cb72be5bd78f8cb76004
Terriakidesu <trra53@gmail.com>
2025-12-24 21:05:16 +0800

    User Models

---
+++ src/models/users.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Role(BaseModel):
+    role_id: int
+    role_name: str
+
+
+class Permission(BaseModel):
+    permission_id: int
+    permission_code: str
+    description: str
+    category: str
+
+
+class User(BaseModel):
+    user_id: int
+    first_name: str
+    last_name: str
+    username: str
+    password: str
+    craeted_at: datetime
+
+
+class Phone(BaseModel):
+    phone_id: int
+    user_id: int
+    phone: str
+
+
+class Email(BaseModel):
+    email_id: int
+    user_id: int
+    email: str
+
+
+class RolePermission(BaseModel):
+    role_id: int
+    permission_id: int
+
+
+class UserRole(BaseModel):
+    user_id: int
+    role_id: int
