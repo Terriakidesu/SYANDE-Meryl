210e58efabdb4e6b5bd5b2efc796df63085e4c48
Terriakidesu <trra53@gmail.com>
2025-12-24 19:05:20 +0800

    routes

--- src/routes/api/__init__.py
+++ src/routes/api/__init__.py
@@ -1,1 +1,4 @@
 from .inventory import inventory_router
+from .management import management_router
+from .users import users_router
+from .sales import sales_router
--- src/server.py
+++ src/server.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 
-from .routes.api import inventory_router
+from .routes.api import (
+    inventory_router,
+    management_router,
+    sales_router,
+    users_router
+)
 
 app = FastAPI()
 
+app.include_router(management_router)
 app.include_router(inventory_router)
+app.include_router(users_router)
+app.include_router(sales_router)
 
 
 @app.get("/")
