cb42c65b71cf50d04561bf0bf4e0332aa11ad060
Terriakidesu <trra53@gmail.com>
2025-12-24 16:48:43 +0800

    database

--- src/includes/database.py
+++ src/includes/database.py
@@ -1,6 +1,25 @@
 import mysql.connector
 
+from typing import Optional
+
+from ..Settings import Settings
+
+
 class Database:
 
     def __init__(self):
-        pass
+        self.connect()
+
+    def connect(self):
+        Settings.reload()
+
+        self.db = mysql.connector.connect(
+            host=Settings.secrets.db_hostname,
+            user=Settings.secrets.db_username,
+            password=Settings.secrets.db_password,
+            database=Settings.secrets.db_database
+        )
+
+    @property
+    def cursor(self):
+        return self.db.cursor
