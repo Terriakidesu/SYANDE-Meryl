e83b35b3769482da15265144f983afea2e156659
Terriakidesu <trra53@gmail.com>
2025-12-25 15:58:43 +0800

    listing endpoints

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from .routes.api import (
     inventory_router,
@@ -18,3 +18,12 @@ app.include_router(sales_router)
 @app.get("/")
 async def home():
     return "Hello"
+
+
+@app.get("/endpoints")
+async def list_endpoints(request: Request):
+    url_list = [
+        f"{route.methods} - {route.path}" for route in request.app.routes
+    ]
+
+    return url_list
