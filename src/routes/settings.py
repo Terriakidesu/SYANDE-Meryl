35e3e0c7bfc43d0c2ed9d4f9687518ffb0818c8f
Terriakidesu <trra53@gmail.com>
2026-01-10 14:51:34 +0800

    feat: Add settings index and profile page templates with routing

--- src/routes/settings.py
+++ src/routes/settings.py
@@ -1,7 +1,21 @@
 
 from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
 
 from ..depedencies import is_authenticated
 
 settings_router = APIRouter(
-    "/settings", dependencies=[Depends(is_authenticated)])
+    prefix="/settings", dependencies=[Depends(is_authenticated)])
+
+
+templates = Jinja2Templates("assets/public/templates/")
+
+
+@settings_router.get("/")
+async def settings_home(request: Request):
+    return templates.TemplateResponse(request, "settings/index.html")
+
+
+@settings_router.get("/profile")
+async def settings_home(request: Request):
+    return templates.TemplateResponse(request, "settings/profile.html")
